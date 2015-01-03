f9206ea9b74e2e432bce3240d73efd225619b76b
KittyMac <rocco@chimerasw.com>
2015-01-03 13:15:56 -0500

    adding a basic <Switcher /> implementation

---
+++ PUSwitcher.cs
@@ -0,0 +1,57 @@
+
+
+using UnityEngine;
+using System.Xml;
+using System.Collections;
+
+public partial class PUSwitcher : PUSwitcherBase {
+
+	private int currentIndex = -1;
+
+	public override void gaxb_complete() {
+		foreach (object childObj in children) {
+			PUGameObject child = childObj as PUGameObject;
+			child.CheckCanvasGroup ();
+			child.canvasGroup.alpha = 0;
+			child.gameObject.SetActive (false);
+		}
+
+		SwitchTo (0);
+	}
+
+
+	public void SwitchTo(int i) {
+		if (currentIndex == i) {
+			return;
+		}
+
+		HideIndex (currentIndex, 0.0f);
+		currentIndex = i;
+		ShowIndex (currentIndex, 0.66f);
+	}
+
+	private void HideIndex(int idx, float delay) {
+		if (idx >= 0 && idx < children.Count) {
+			PUGameObject child = children [idx] as PUGameObject;
+
+			child.gameObject.SetActive (true);
+			child.canvasGroup.alpha = 1;
+			LeanTween.alpha (child.gameObject, 0.0f, 1.33f).setEase (LeanTweenType.easeOutCubic).setDelay(delay).setOnComplete (() => {
+				child.gameObject.SetActive (false);
+			});
+		}
+	}
+
+	private void ShowIndex(int idx, float delay) {
+		if (idx >= 0 && idx < children.Count) {
+			PUGameObject child = children [idx] as PUGameObject;
+
+			child.gameObject.SetActive (true);
+			child.canvasGroup.alpha = 0;
+			LeanTween.alpha (child.gameObject, 1.0f, 1.33f).setEase (LeanTweenType.easeOutCubic).setDelay(delay).setOnComplete (() => {
+
+			});
+		}
+	}
+
+}
