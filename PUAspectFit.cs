4e0f6ddf16b157e598516a9f916e072cc2bc9c92
KittyMac <rocco@chimerasw.com>
2015-06-24 17:43:22 -0400

    changed ApsectFit to use unity's aspect fitter

--- PUAspectFit.cs
+++ PUAspectFit.cs
@@ -1,47 +1,16 @@
-
-
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-
-
-
-public class AspectRatioContentSize : MonoBehaviour
-{
-	public Vector2 contentSize;
-
-	private RectTransform rectTransform;
-
-	protected void LateUpdate()
-	{
-		rectTransform = gameObject.transform as RectTransform;
-		float parentWidth = rectTransform.rect.size.x;
-		float parentHeight = rectTransform.rect.size.y;
-
-		if (parentWidth > 0 && parentHeight > 0) {
-			float scaleX = parentWidth / contentSize.x;
-			float scaleY = parentHeight / contentSize.y;
-			if (scaleX > scaleY) {
-				this.rectTransform.localScale = new Vector2 (scaleX, scaleX);
-			} else {
-				this.rectTransform.localScale = new Vector2 (scaleY, scaleY);
-			}
-		}
-
-	}
-}
-
 
 public partial class PUAspectFit : PUAspectFitBase {
 
-	AspectRatioContentSize fitter = null;
+	AspectRatioFitter fitter = null;
 
 	public override void gaxb_init ()
 	{
 		gameObject = new GameObject ("<AspectFit/>", typeof(RectTransform));
 
-		gameObject.AddComponent<AspectRatioContentSize> ();
-		fitter = gameObject.GetComponent<AspectRatioContentSize> ();
-		fitter.contentSize = contentSize.Value;
+		fitter = gameObject.AddComponent<AspectRatioFitter> ();
+		fitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+		fitter.aspectRatio = contentSize.Value.x / contentSize.Value.y;
 	}		
 }
